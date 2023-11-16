da9ffe3b3e8fe914bbade221f63ef6f61fca399a
Pablo Oliveira <48020553+pabloolvr@users.noreply.github.com>
2023-11-16 17:40:30 -0300

    Added Item object

---
+++ Source/SoulsLike/Private/Items/Item.cpp
@@ -0,0 +1,54 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Items/Item.h"
+#include "Components/SphereComponent.h"
+
+AItem::AItem()
+{
+    PrimaryActorTick.bCanEverTick = true;
+    ItemMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("ItemMeshComponent"));
+    RootComponent = ItemMesh;
+    Sphere = CreateDefaultSubobject<USphereComponent>(TEXT("Sphere"));
+    Sphere->SetupAttachment(GetRootComponent());
+}
+void AItem::BeginPlay()
+{
+    Super::BeginPlay();
+
+    // add start/end overlap events
+    Sphere->OnComponentBeginOverlap.AddDynamic(this, &AItem::OnSphereOverlap);
+    Sphere->OnComponentEndOverlap.AddDynamic(this, &AItem::OnSphereEndOverlap);
+}
+
+float AItem::TransformedSin()
+{
+    return Amplitude * FMath::Sin(RunningTime * TimeConstant);
+}
+float AItem::TransformedCos()
+{
+    return Amplitude * FMath::Cos(RunningTime * TimeConstant);
+}
+void AItem::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+    const FString OtherActorName = OtherActor->GetName();
+    if (GEngine)
+    {
+        GEngine->AddOnScreenDebugMessage(1, 30.f, FColor::Red, OtherActorName);
+    }
+}
+
+void AItem::OnSphereEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+    const FString OtherActorName = FString("Ending Overlap with: ") + OtherActor->GetName();
+    if (GEngine)
+    {
+        GEngine->AddOnScreenDebugMessage(1, 30.f, FColor::Blue, OtherActorName);
+    }
+}
+
+void AItem::Tick(float DeltaTime)
+{
+    Super::Tick(DeltaTime);
+    RunningTime += DeltaTime;
+}
---
+++ Source/SoulsLike/Public/Items/Item.h
@@ -0,0 +1,50 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Item.generated.h"
+
+class USphereComponent;
+
+UCLASS()
+class SOULSLIKE_API AItem : public AActor
+{
+	GENERATED_BODY()
+	
+public:
+    AItem();
+    virtual void Tick(float DeltaTime) override;
+protected:
+    virtual void BeginPlay() override;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Sine Parameters")
+    float Amplitude = 0.25f;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Sine Parameters")
+    float TimeConstant = 5.f;
+    UFUNCTION(BlueprintPure)
+    float TransformedSin();
+    UFUNCTION(BlueprintPure)
+    float TransformedCos();
+    template<typename T>
+    T Avg(T First, T Second);
+    UFUNCTION()
+    void OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+    UFUNCTION()
+    void OnSphereEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+
+private:
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+    float RunningTime;
+    UPROPERTY(VisibleAnywhere)
+    UStaticMeshComponent* ItemMesh;
+    UPROPERTY(VisibleAnywhere)
+    USphereComponent* Sphere;
+};
+
+template<typename T>
+inline T AItem::Avg(T First, T Second)
+{
+    return (First + Second) / 2;
+}
